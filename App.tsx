6a143775063c2a4c7e552d4b8477cddc5f2ca424
Alexandre <alexandrepereiraassano@gmail.com>
2024-09-30 20:03:05 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Hello } from './src/components/Hello';
 
-export default function App() {
+const  App = ():JSX.Element => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Hello bang>World</Hello>
+      <Hello style={{ fontSize: 16 }}>World</Hello>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
