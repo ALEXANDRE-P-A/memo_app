0757d69195e56ed0ccf8e172d18789beeb609a44
Alexandre <alexandrepereiraassano@gmail.com>
2024-10-01 14:59:46 +0900

    memolist page modeling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,68 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = ():JSX.Element => {
 
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={ styles.container }>
+      {/* Header(starts here) */}
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* Memo List */}
+      <View>
+
+        {/* Memo List Item */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年10月01日 14:49</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* Memo List Item */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年10月01日 14:49</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* Memo List Item */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年10月01日 14:49</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>＋</Text>
+      </View>
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
 
 export default Index;
